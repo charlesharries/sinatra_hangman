bf2cb5b4b1612f2cef5f10fa63a5d33d81d65ff2
Charles Harries <charlesgharries@gmail.com>
2016-11-18 16:26:42 -0500

    finish app and deploy to heroku

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './sinatra_hangman'
+run Sinatra::Application
--- sinatra_hangman.rb
+++ sinatra_hangman.rb
@@ -1,47 +1,109 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-def generate_word
-  answer = ""
-  loop do
-    answer = @lines[Random.rand(@lines.length)].strip # The file is full of secret whitespace!
-    break if answer.length >= 5 && answer.length <= 12
-  end
-  answer
-end
-
-def get_correct_indices(letter)
-  if letter.empty?
-    []
-  elsif ANSWER.downcase.include?(letter)
-    # answer.downcase[i] here to handle if one of the letters in answer is a capital
-    ANSWER.size.times.select { |i| ANSWER.downcase[i] == letter }
-  else
-    []
-  end
-end
-
-def make_string(correct_indices)
-  string = ""
-  ANSWER.length.times do |index|
-    if correct_indices.include?(index)
-      string << ANSWER[index] + " "
-    else
-      string << "_ "
-    end
-  end
-  string
-end
-
-@lines = File.readlines("5desk.txt")
-@@tries = 10
-@@correct_indices = []
-@@incorrect_characters = []
-ANSWER = generate_word
+enable :sessions
+set :session_secret, '(*Yjijiwefnweewwef94523'
 
 get '/' do
-  letter = params['guess'].nil? ? "" : params['guess']
-  @@correct_indices += get_correct_indices(letter)
-  word_string = make_string(@@correct_indices)
-  erb :index, :locals => {:answer => ANSWER, :tries => @@tries, :letter => letter, :indices => @@correct_indices, :string => word_string}
+  game_setup
+  @answer = session[:answer]
+  @correct_indices = session[:correct_indices]
+  @incorrect_characters = session[:incorrect_characters]
+  @tries = session[:tries]
+  message = nil
+  erb :index, :locals => {:message => message}
+end
+
+post '/' do
+  @answer = session[:answer]
+  @correct_indices = session[:correct_indices]
+  @incorrect_characters = session[:incorrect_characters]
+  @tries = session[:tries]
+  letter = params['guess'].downcase.nil? ? "" : params['guess']
+  if good_guess?(letter)
+    update_game(letter)
+  else
+    message = "That's not a good fucking guess. Try that one again, champ."
+  end
+  erb :index, :locals => {:message => message}
+end
+
+get '/win' do
+  erb :win
+end
+
+get '/lose' do
+  erb :lose
+end
+
+helpers do
+
+  def game_setup
+    session[:answer] = generate_word
+    session[:correct_indices] = []
+    session[:incorrect_characters] = []
+    session[:tries] = 10
+  end
+
+  def generate_word
+    @lines = File.readlines("5desk.txt")
+    answer = ""
+    loop do
+      answer = @lines[Random.rand(@lines.length)].strip # The file is full of secret whitespace!
+      break if answer.length >= 5 && answer.length <= 12
+    end
+    answer
+  end
+
+  def good_guess?(letter)
+    if letter?(letter) && !already_guessed?(letter)
+      true
+    else
+      false
+    end
+  end
+
+  def letter?(letter)
+    return letter =~ /[A-Za-z]/ && letter.length == 1 ? true : false
+  end
+
+  def already_guessed?(letter)
+    return @incorrect_characters.include?(letter) ? true : false
+  end
+
+  def get_correct_indices(letter)
+    if letter.empty?
+      []
+    elsif @answer.downcase.include?(letter)
+      # answer.downcase[i] here to handle if one of the letters in answer is a capital
+      @answer.size.times.select { |i| @answer.downcase[i] == letter }
+    else
+      []
+    end
+  end
+
+  def update_game(guess)
+    session[:correct_indices] += get_correct_indices(guess)
+    session[:correct_indices].uniq! # So you don't get [0, 5, 2, 2, 2] or something
+    if get_correct_indices(guess).empty?
+      session[:tries] -= 1
+      session[:incorrect_characters] << guess
+    end
+    if winner?
+      redirect '/win'
+    end
+    if session[:tries] == 0
+      redirect '/lose'
+    end
+  end
+
+  def winner?
+    # Crude but it works
+    if session[:correct_indices].length == @answer.length
+      true
+    else
+      false
+    end
+  end
+
 end
